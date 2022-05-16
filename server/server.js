f782c5c59ddf71f11797412a4d90d40d8658fd2f
Pandora <benjax48@hotmail.com.ar>
2022-05-16 00:01:27 -0300

    qr

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const {Server, Socket} = require('socket.io')
+const SocketServer = require('websocket').server
 const http = require('http')
 
 
@@ -13,8 +13,7 @@ const clientRoute = require("./routes/client")
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server)
-
+wsServer = new SocketServer({httpServer:server})
 
 app.use(express.json())
 
@@ -23,9 +22,66 @@ app.use("/api/details", detailsRoute);
 app.use("/api/shop", shopRoute); 
 app.use("/api/client", clientRoute); 
 
-io.on('connection', (data)=>{
-    console.log(`Nuevo usuario conectado  ${data.id} ${data.client.conn.server.clientsCount} usuarios conectados!`)
-    io.emit("c", `${data.id}`) 
+
+
+
+const connections = []
+
+wsServer.getUniqueID = function () {
+  function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4();
+};
+
+
+wsServer.on('request', (req) => {
+    const connection = req.accept()
+    const {device} = req.resourceURL.query
+    const {key} = req.resourceURL.query;
+    const {shopkey} = req.resourceURL.query;
+
+
+    connection.id = key
+    connection.reciver = shopkey
+    connection.device = device
+
+    if(device == "app"){
+      
+      console.log("Se conecto desde la app.... "+connection.id);
+    }else if(device == "web"){
+      console.log("Se conecto desde el web.... "+connection.id);
+    }
+
+
+    connections.push(connection)
+    //const parsed = JSON.parse(mes.utf8Data)
+    connection.on('message', (mes) => {
+      try{
+        console.log(connection.device)
+        if(connection.device == "app"){
+          console.log('El mensaje viene de '+ connections[connections.findIndex(x => x.id === key)].id+" y se dirige a "+connections[connections.findIndex(x => x.id === key)].reciver)
+          const to_send = connections[connections.findIndex(x => x.id === connections[connections.findIndex(x => x.id === key)].reciver)]
+          console.log(to_send.id)
+          to_send.send(mes.utf8Data)
+        }else if(connection.device == "web"){
+          const parsed = JSON.parse(mes.utf8Data);
+          console.log("parceritox d"+parsed);
+          console.log('El mensaje viene de '+ " y se dirige a "+parsed["send_to"])
+          const to_send = connections[connections.findIndex(x => x.id === parsed["send_to"])]
+          to_send.sendUTF(JSON.stringify(parsed["data"]));
+          //to_send.sendUTF(JSON.stringify(parsed["data"]))
+        }
+
+      }catch(err){}
+       
+    })
+  
+    connection.on('close', (resCode, des) => {
+        console.log('connection closed')
+        connections.splice(connections.indexOf(connection), 1)
+    })
+  
 })
 
 
@@ -33,6 +89,7 @@ mongoose
     .connect(process.env.URI, { useNewUrlParser: true })
 	.then(() => { server.listen(process.env.PORT || 3000, (socket) => console.log('BoltPay its alive')) })
 
+app.get('/testqr', function(req,res) { res.sendFile( '/public/qr.html', { root: process.cwd() }); });
 
 app.get('/', (req, res) => { res.redirect('/api') });
 
